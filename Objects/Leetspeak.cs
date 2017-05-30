c63a87f7bd1d89a612a7ab95dc9254c25deb99a7
Andrew Glines and Jordan Mysliwiec <andrew+jordan@>
2017-05-30 11:04:35 -0700

    Add first spec to README.md, write code to pass spec

--- Objects/Leetspeak.cs
+++ Objects/Leetspeak.cs
@@ -1,11 +1,23 @@
+using System;
+
 namespace Leetspeak.Objects
 {
   public class LeetspeakTranslator
   {
-    public string Translate(string UserInput)
-    {
-      return "";
-    }
 
+    public string Translate(string userInput)
+    {
+      if (userInput.Contains("e"))
+      {
+        string output = userInput.Replace("e", "3");
+        Console.WriteLine(output);
+        return output;
+      }
+      else
+      {
+        return "did not contain e";
+      }
+
+      }
+    }
   }
-}
